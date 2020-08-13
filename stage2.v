005539e1ea6673e7c92f445e16d69c98a416b594
kamran <kamran2727@gmail.com>
2020-08-13 23:25:33 +0430

    stage 2 completed

--- stage2.v
+++ stage2.v
@@ -1,111 +1,71 @@
-module stage2(clk, rst, init, instruction, regFileWriteData, stackOut, stackIn, readData1, readData2, Z, C, AluInputBSel, ALUfunction, shiftCount, pcAdderInputASel, pcInputSel, const_disp, dest, writeAddress, DMMemWrite, ControllerOutput_regFileWriteDataSel, LDM, controllerInput_regWrite, Cenb, Zenb, r1Address, r2Address, sstall);
+module stage2(clk, rst, init, instruction, PCIn, MEM_Dest, MEM_WB_EN, EXE_Dest, EXE_WB_EN, BlackWire, Hazard, WB_Dest, WB_Value, WB_WB_EN,
+	WB_EN, MEM_R_EN, MEM_WB_EN, B, S, SregOut, two_src, OutImm, ShiftOperandOut, SignImm24Out, DestOut);
 	
-	//controller signals:
-	//stage 1 signals:
-	output pcAdderInputASel;
-	output [1:0] pcInputSel;
-	//stage 2 signals:
-	wire push, pop;
-	// wire selectR2;
-	wire registerFileR2DataSel;
+	input clk, rst, init;
+	input [31:0] instruction, PCIn;
+	input MEM_WB_EN, EXE_WB_EN, WB_WB_EN, SregOut;
+	input [3:0] MEM_Dest, EXE_Dest;
+	input [3:0] WB_Dest;
+	input Hazard;
+	wire ControllerOutput;
+	output WB_EN, MEM_R_EN, MEM_W_EN, B, S, two_src, OutImm, SignImm24Out;
+	output [3:0] EXE_CMD, DestOut;
+	output [11:0] ShiftOperandOut;
+	output [31:0] PCout;
+	wire ConditionCheckOut;
+	output [31:0] Val_Rn, Val_Rm;
 
-	// stage 3 signals:
-	output AluInputBSel;
-	output [3:0] ALUfunction;
-	output Cenb, Zenb;
-	// stage 4 signals:
-	output DMMemWrite;
-	// stage 5 signals:
+	assign  PCout = PCIn;
+	assign two_src = MEM_W_EN | (^ (instruction[25]));
+	assign OutImm = instruction[25];
+	assign ShiftOperandOut = instruction[11:0];
+	assign SignImm24Out = instruction[25];
+	assign DestOut = Rd;
 
-	output ControllerOutput_regFileWriteDataSel;
-	output LDM;
-
-
-	wire [18:0] controllerAllBits;
-	wire ControllerOutputStall;
-	
-	wire BadRegOutput;
-	register #(.size(1)) BadReg(
-		.clock(clk),
-		.reset(rst),
-		.regIn(ControllerOutputStall),
-		.regOut(BadRegOutput)
-	);
-
-	mux_2_input  #(.WORD_LENGTH (19)) controllerInstructionMux (    //mux 8
-		.in1(instruction), 
-		.in2({6'b111101,13'd0}), 
-		.sel(sstall | BadRegOutput), 
-		.out(controllerAllBits)
-	);
 
 	controller CU (
 		.init_signal(init), 
 		.clock(clk), 
-		.allBits(controllerAllBits), 
-		.Zero(Z), 
-		.CarryOut(C), 
-		.regFileWriteDataSel(ControllerOutput_regFileWriteDataSel), 
-		.selectR2(registerFileR2DataSel), 
-		.AluInputBSel(AluInputBSel), 
-		.ALUfunction(ALUfunction), 
-		.STM(DMMemWrite), 
-		.LDM(LDM), 
-		// .enablePC(pcEnb), 
-		.enableZero(Zenb), 
-		.enableCarry(Cenb), 
-		.pcAdderInputASel(pcAdderInputASel), 
-		.push(push), 
-		.pop(pop), 
-		.pcInputSel(pcInputSel),
-		.stall(ControllerOutputStall)
+		.S(instruction[20]),
+		.OPCode(instruction[24:21]),
+		.Mode(instruction[27:26]),
+		.output(ControllerOutput)
 	);
 
-	input sstall;
-	input clk, rst, init;
-	input [7:0] regFileWriteData;
-	output [7:0] readData2, readData1;
-	input [18:0] instruction;
-	input [11:0] stackIn;
-	output [11:0] stackOut;
-	input C, Z;
-	output [2:0] shiftCount;
-	output [7:0] const_disp;
-	output [2:0] dest;
-	input [2:0] writeAddress;
-	input controllerInput_regWrite;
-	wire [2:0] registerFileR2Data;
-	output [2:0] r1Address, r2Address;
-	assign r1Address = instruction[10:8];
-	assign r2Address = registerFileR2Data;
-	assign dest = instruction[13:11];
-	assign shiftCount = instruction[7:5];
-	assign const_disp = instruction[7:0];
+	ConditionCheck CCHECK(
+		.cond(instruction[31:28]),
+		.stRegister(SregOut),
+		.out(ConditionCheckOut)
+	);
+	
+	mux_2_input  #(.WORD_LENGTH (4)) MUX3 (    //mux 8
+		.in1(ControllerOutput), 
+		.in2(0), 
+		.sel(Hazard | (^ConditionCheckOut)), 
+		.out({WB_EN, MEM_R_EN, MEM_W_EN, EXE_CMD, B, S})
+	);
+
+	wire [3:0] Rn, Rd, Rm;
+	assign Rn = instruction[19:16];
+	assign Rd = instruction[15:12];
+	assign Rm = instruction[3:0];
+
+	mux_2_input  #(.WORD_LENGTH (4)) MUX2 (    //mux 8
+		.in1(Rd), 
+		.in2(Rm), 
+		.sel(MEM_W_EN), 
+		.out(controllerAllBits)
+	);
 
 	registerFile regFile(
 		.clock(clk), 
-		.regWrite(controllerInput_regWrite), 
-		.writeRegister(writeAddress), 
-		.writeData(regFileWriteData), 
-		.readRegister1(r1Address), 
-		.readRegister2(registerFileR2Data), 
-		.readData1(readData1), 
-		.readData2(readData2)
-	);
-
-	mux_2_input  #(.WORD_LENGTH (3)) registerFileR2DataMux (    //mux 5
-		.in1(instruction[7:5]), 
-		.in2(instruction[13:11]), 
-		.sel(registerFileR2DataSel), 
-		.out(registerFileR2Data)
-	);
-
-	stack  ST(
-		.clock(clk), 
-		.push(push), 
-		.pop(pop), 
-		.result(stackOut), 
-		.in(stackIn)
+		.regWrite(WB_WB_EN), 
+		.writeRegister(WB_Dest), 
+		.writeData(WB_Value), 
+		.readRegister1(Rn), 
+		.readRegister2(Rd), 
+		.readData1(Val_Rn), 
+		.readData2(Val_Rm)
 	);
 
 endmodule 
