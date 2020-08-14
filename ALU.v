44125ce5d7ffbe23ade0c7fabf9513273160f7e3
arapa <a.rparsayei1998@yahoo.com>
2020-08-14 18:12:48 +0430

    ALU added

--- ALU.v
+++ ALU.v
@@ -1,36 +1,39 @@
-module ALU(inputA, inputB, carryIn, operation, result, carryOut, zero, shiftCount);
-	input [7:0] inputA, inputB;
-	input carryIn;
-	input [3:0] operation;
-	output reg [7:0] result;
-	output reg carryOut, zero;
-	input [2:0] shiftCount;
 
-	integer i;
+module ALU(inputA, inputB, exeCommand, carryIn, result, statusOut);
+	input[31:0] inputA, inputB;
+	input[3:0] exeCommand;
+	input carryIn;
+	output reg[31:0] result;
+	output[3:0] statusOut;
+
+	reg negative, zero, carry, overflow;
+
 	always @(*) begin
-		result = 8'b0;	
-		case(operation)
-			4'b1000: {carryOut, result} = inputA + inputB;
-			4'b1001: {carryOut, result} = inputA + inputB + carryIn;
-			4'b1010: {carryOut, result} = inputA - inputB;
-			4'b1011: {carryOut, result} = inputA - inputB - carryIn;
-			4'b1100: result = inputB & inputA;
-			4'b1101: result = inputB | inputA;
-			4'b1110: result = inputB ^ inputA;
-			4'b1111: result = inputB &^ inputA;
-			4'b0000: {carryOut ,result} <= (result << shiftCount);
-			4'b0001: {result, carryOut} <= (result >> shiftCount);
-			4'b0010: 
-					for ( i = 0; i < shiftCount; i = i + 1) begin
-						result = {result[6:0], result[7]};
-					end
-			4'b0011:
-					for ( i = 0; i < shiftCount; i = i + 1) begin
-						result = {result[0], result[7:1]};
-					end
-			default: result = result;
-		endcase
-		zero = ~(|result);
+		result <= 32'b 0;
+		negative <= 1'b 0;
+		zero <= 1'b 0;
+		carry <= 1'b 0;
+		overflow <= 1'b 0;
+
+		case(exeCommand)
+			4'b 0001: result = inputB; //MOV
+			4'b 1001: result = ~inputB; //MVN
+			4'b 0010: {carry,result} = inputA + inputB; //ADD
+			4'b 0011: {carry,result} = inputA + inputB + carryIn; //ADC
+			4'b 0100: {carry,result} = inputA - inputB; //SUB
+			4'b 0101: {carry,result} = inputA - inputB - 1; //SBC
+			4'b 0110: result = inputA & inputB; //AND
+			4'b 0111: result = inputA | inputB; //ORR
+			4'b 1000: result = inputA ^ inputB; //EOR
+			4'b 0100: {carry,result} = inputA - inputB; //CMP
+			4'b 0110: result = inputA & inputB; //TST
+			4'b 0010: {carry,result} = inputA + inputB; //LDR, STR
+		endcase	
+		zero <= result == 32'b 0;
+		negative <= result[31];
+		overflow <= (inputA[31] && inputB[31] && ~negative) || (~inputA[31] && ~inputB[31] && negative);
 	end
 
-endmodule
+	assign statusOut = {negative,zero,carry,overflow};
+
+endmodule 
